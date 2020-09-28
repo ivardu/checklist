31b5207d751fa2e972dee3606e7442fac4101397
Ravi Kumar <ivardu19@gmail.com>
2020-09-28 13:55:12 +0530

    Changes to the ticklistt

--- template/forms.py
+++ template/forms.py
@@ -4,7 +4,7 @@ from django import forms
 
 
 class TemplateForm(forms.ModelForm):
-	title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder':'Title', 'id':'js_template_title'}))
+	title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder':'Title', 'id':'js_template_title', 'class':'form-control no-border'}))
 	class Meta:
 		model = Template
 		fields = ['title']
